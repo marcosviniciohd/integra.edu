597265e84aa79938ed322399bfec31bb22dc8974
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2025-02-18 22:54:18 -0300

    feat: criar estrutura web-api, criar models

---
+++ src/main/java/br/com/integra/edu/model/Questao.java
@@ -0,0 +1,25 @@
+package br.com.integra.edu.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Document(collection = "questoes")
+public class Questao {
+    @Id
+    private String id;
+    private String titulo;
+    private String enunciado;
+    private UUID temaId;
+    private UUID disciplinaId;
+    private List<UUID> alternativasId;
+    private Boolean visivel;
+    private LocalDate dataCriacao;
+    private LocalDate dataAtualizacao;
+    private Boolean ativo;
+}
